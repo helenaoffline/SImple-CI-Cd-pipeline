45ebb246e4726a4a0ae3ac140595ca7df23a95ff
helenaoffline <helena.mhms2005@gmail.com>
2025-12-01 17:38:26 -0300

    add count

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,14 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+            System.out.println(i);
+            for (int j = 1; j <= 5; j++) {
+                System.out.println("j = " + j);
+                System.out.println("Banana fan");
+            }
+
+           // mimosa com narget
         }
     }
 }
